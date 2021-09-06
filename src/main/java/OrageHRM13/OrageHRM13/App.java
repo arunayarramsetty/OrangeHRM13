385b8401d5055782a820965c29522bd840d2c349
arunayarramsetty <aruna2602@gmail.com>
2021-09-06 10:10:59 +0530

    committing asdasdas

---
+++ src/main/java/OrageHRM13/OrageHRM13/App.java
@@ -0,0 +1,13 @@
+package OrageHRM13.OrageHRM13;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
